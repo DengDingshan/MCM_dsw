38980ca2ee2240543412d4c2064e13cd9f19b710
Wang-Zelin <719507902@qq.com>
2018-02-08 15:01:19 +0800

    wzl

--- MCM_wzl/TSP/TSP-BFS.py
+++ MCM_wzl/TSP/TSP-BFS.py
@@ -1,4 +1,41 @@
 import copy
+class SQueue():
+	def __init__(self, init_len=8):
+		self._len = init_len 
+		self._elems = [0]*init_len
+		self._head = 0
+		self._num = 0
+
+	def is_empty(self):
+		return self._num == 0
+
+	def peek(self):
+		if self._num == 0 :
+			raise QueueUnderflow 
+		return self._elems[self._head]
+
+	def dequeue(self):
+		if self._num == 0 :
+			raise QueueUnderflow 
+		e = self._elems[self._head]
+		self._head = (self._head + 1 ) % self._len 
+		self._num -= 1
+		return e 
+
+	def enqueue(self, e):
+		if self._num == self._len :
+			self._extend()
+		self._elems[(self._head+self._num) % self._len] = e
+		self._num += 1 
+
+	def __extend(self):
+		old_len = self._len
+		self._len *= 2
+		new_elems = [0]*self._len
+		for i in range(old_len):
+			new_elems[i] = self._elems[(self._head + i)% old_len]
+		self. elems, self._head = new_elems, 0
+
 
 def get_down(c_s):
 	summ = 0;
@@ -21,16 +58,54 @@ def get_up(graph,used):
 		summ += minlen
 	return summ + graph[pos][0]
 
-def solver(graph, used, pos, uplim, downlim):
-	st = [0]
-	for i in range(len(graph)-1) :
-		st
-	return s
-	
-def value(graph,st):
-	val = 0
-	for i in range(len(graph)):
+def solver(graph, pos, uplim, downlim):
+	val = 0; nextp = [0]
+	qu = SQueue()
+	qu.enqueue([0])
+	while not qu.is_empty():
+		pos = qu.dequeue()
+		print pos
+		for i in range(len(graph)):
+			if not i in pos:
+				nextp = pos + [i]
+				val = value(graph,nextp)
+				print nextp,val
+			if len(nextp) == len(graph) :
+				print 'binggo',val
+				return
+			if val<=uplim and val>=downlim :
+				qu.enqueue(nextp)
+				print 'a sign'
+	print("No path.")
 	return 
+	
+def value(graph,np):
+	val = 0
+	temp = float('inf')
+	anti_np = []
+	for i in range(len(graph)):
+		if not i in np:
+			anti_np.append(i)
+	for i in range(len(np)-1):
+		val += graph[np[i]][np[i+1]]*2.
+
+	for i in anti_np:
+		if temp>graph[np[0]][i]:
+			temp = graph[np[0]][i]
+	val += temp; temp = float('inf');
+
+	for i in anti_np:
+		if temp>graph[np[-1]][i]:
+			temp = graph[np[-1]][i]
+	val += temp
+
+	for i in anti_np:
+		temp = [];
+		for j in range(len(graph)):
+			temp.append(graph[i][j])
+		temp.sort()
+		val += temp[0] + temp[1] 
+	return val/2.
 
 
 graph = open ("TSP.txt","r").read().split('\n')
@@ -46,5 +121,9 @@ c_s = copy.deepcopy(graph)
 uplim = get_up(graph,used)
 downlim = get_down(c_s)
 
+# for i in range(len(graph)):
+# 	used(i) = True 
+
 print uplim,downlim
-solver(graph, used, pos, uplim, downlim);
+
+solver(graph, 1, uplim, downlim);
