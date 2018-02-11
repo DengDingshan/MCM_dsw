85470b0ddf0f623542967fe880b3bf0f2118f1e3
qy-sunyang <455151128@qq.com>
2018-02-11 17:12:53 +0800

    1/11

--- mcm_sy/densitychangeinyear.m
+++ mcm_sy/densitychangeinyear.m
@@ -1,21 +1,21 @@
 % density change in year
 clc;close all;clear;
-densitychangeinyear=importdata('100kmdensitychangeinyear.csv',',',1);
-densitychangeinyear=densitychangeinyear.data;
+densitychangeinyears=importdata('100kmdensitychangeinyear.csv',',',1);
+densitychangeinyears=densitychangeinyears.data;
 monthchange=zeros(12,1);
-month=densitychangeinyear(1:end,1);
+month=densitychangeinyears(1:end,1);
 for i=1:12
-    monthchange(i,1)=mean(densitychangeinyear(i,4:8));
+    monthchange(i,1)=mean(densitychangeinyears(i,4:8));
 end
 
 
 figure(1)
 hold on
-scatter(month,monthchange)
+scatter(month,monthchange,'k*')
 [P,res] = polyfit(month,monthchange,6);
 month2=1:0.1:12;
 monthchange2=polyval(P,month2);
-plot(month2,monthchange2,'r')
+plot(month2,monthchange2,'b')
 hold off
 n=res.normr;
 y=sum((monthchange-mean(monthchange)).^2);
