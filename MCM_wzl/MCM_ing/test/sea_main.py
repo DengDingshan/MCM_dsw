e58bfbcb4ff5141dca3cb10b7f446fbbd4c60c55
DengDingshan <ddskevin@qq.com>
2018-02-12 16:13:45 +0800

    dds

--- MCM_wzl/MCM_ing/test/sea_main.py
+++ MCM_wzl/MCM_ing/test/sea_main.py
@@ -1,4 +1,4 @@
-import reflection_parameter as Ocean 
+import reflection_parameter as Ocean
 import numpy as np
 import params as bp
 import generate as bg
@@ -10,14 +10,14 @@ f = 13.			# MHz
 hrms = 2.4		# waves' RMS heights
 a = Ocean.parameter(theta, f, hrms)
 reflection = a.reflection()
-print reflection
+print (reflection)
 import matplotlib.pyplot as plt
 
 for ii in range (11):
 	temp = []
 	plt.figure()
 	for i in range(1000):
-		
+
 		# parameters about the smooth aera rate
 		hrms = 2.4		# pay attention to be same
 		hurst = 0.1*ii+0.5		# characterization of the smoothness of waves
@@ -26,12 +26,10 @@ for ii in range (11):
 		surface_sample = bg.self_affine(surface_params, N_power_of_two)
 		smooth = sc.run(surface_sample)
 		temp.append(np.log10(smooth * reflection)*10)
-		print i
+		print (i)
 
-	plt.hist(temp, 50, normed=0, facecolor='green', alpha=0.5) 
+	plt.hist(temp, 50, normed=0, facecolor='green', alpha=0.5)
 	plt.savefig("hist "+str(hurst)+".png")
-	
+
 # print smooth
 # print np.log10(smooth * reflection)*10
-
-
